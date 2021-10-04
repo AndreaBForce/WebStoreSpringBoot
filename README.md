bae5e3c305ea61e4acef98d9e06facbb03dff002
Andrea Riccardi <andrea.riccardi@student.supsi.ch>
2021-10-04 18:07:37 +0200

    Serie02 - Servlet rev
    Consegna serie02
    Postman: Param,Raw json, application/x-www-form-urlencoded

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 riccardi
 
 Serie 02) 
-Il server va sulla /annunci/Item e per mandare le cose con POST utiliyyo Postman
+Il server va sulla /annunci/Item e per mandare le cose con POST utilizzo Postman
+Acceta quindi da post man sia da Param, che da application/x-www-form-urlencoded che da Raw JSON
 http://localhost:8080/annunci/Item per vedere la GET
 Per poter modificare la /annunci/ bisogna andare nella configurazione e Run -> Edi Configuration -> Deployment -> Application Context e li cambi
--- web/src/main/java/ch/supsi/webapp/ItemServlet.java
+++ web/src/main/java/ch/supsi/webapp/ItemServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,10 +30,26 @@ public class ItemServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //oggetti.add(new Item(req.getParameter("title"),req.getParameter("description"),req.getParameter("author")));
-        //resp.getWriter().println("{\n\"title\": "+req.getParameter("title")+"\n\"description\": "+req.getParameter("description")+"\n\"author\": "+req.getParameter("author")+"\n}");
         ObjectMapper mapper = new ObjectMapper();
-        Item letto = mapper.readValue("{\"title\":\""+req.getParameter("title")+"\",\"description\":\""+req.getParameter("description")+"\",\"author\":\""+req.getParameter("author")+"\"}",Item.class);
+        Item letto;
+
+        //gestisco la parte di richiesta in caso sia Raw ovvero che mi arriva json con postman
+        String contType = req.getContentType();
+
+        if(contType!= null && contType.equals("application/json")){
+            StringBuffer jb = new StringBuffer();
+            String line = null;
+            try {
+                BufferedReader reader = req.getReader();
+                while ((line = reader.readLine()) != null)
+                    jb.append(line);
+            } catch (Exception e) { /*report an error*/ }
+
+            letto = mapper.readValue(jb.toString(),Item.class);
+        }else{
+            letto = mapper.readValue("{\"title\":\""+req.getParameter("title")+"\",\"description\":\""+req.getParameter("description")+"\",\"author\":\""+req.getParameter("author")+"\"}",Item.class);
+        }
+
         oggetti.add(letto);
 
         String jsonString = mapper.writeValueAsString(letto);
